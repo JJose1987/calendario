42eab3b1e5aceb13bdcccdda7f27091ee132786a
Juan José <95169921+JJose1987@users.noreply.github.com>
2024-05-20 14:01:41 +0200

    20240520.1401
    20240520.1401

---
+++ functions.js
@@ -0,0 +1,48 @@
+/* JavaScript */
+/* https://www.degraeve.com/reference/urlencoding.php */
+// Variables
+const __Objeto = new Objeto();
+
+// Funciones
+function main() {
+    //Si es telefono girar la vista y las imagenes
+    this.__Objeto = new Objeto();
+    this.__Objeto.sets();
+
+    $('#image-00').change(function(e) {setColors(e, 0)});
+
+    update();
+}
+
+// Asignar valor del campo
+function set(object) {
+    var index = $(object).attr('name');
+    this.__Objeto.set(index, $(object).val());
+
+    update();
+}
+
+// Actualizar valores de la clase
+function update() {
+
+}
+
+// Asignar el valor a los colores
+function setColors(e, i) {
+    var file = e.target.files[0];
+    var reader = new FileReader();
+
+    reader.onload = function(event) {
+        var img = new Image();
+        img.onload = function() {
+            var colorThief = new ColorThief();
+
+            console.log(colorThief.getPalette(img, 5));
+        };
+        
+        img.src = event.target.result;
+    };
+    
+    reader.readAsDataURL(file);
+}
+
