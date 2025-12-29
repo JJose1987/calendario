f45f457186ece98670129c6316fbd0de7f86c2b5
Juan José <95169921+JJose1987@users.noreply.github.com>
2025-12-29 13:24:57 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -787,7 +787,7 @@ function drawMoon(ctx, color, x, y, size, fase) {
 
 // Dibujar el cnv
 function draw(cnv) {
-    var q = 22;
+    var q = 10;
     cnv.width  = q * 595;
     cnv.height = q * 842;
 
@@ -1005,3 +1005,4 @@ function draw(cnv) {
         ctx.fillText(kwargs['year'], (x * (w / 8)) + 10, ((y + 1) * (h / 15)) + (h / 2));
     }
 }
+
